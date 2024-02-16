4ca074caf5854d628cdd4d570b59f4358f77c6db
Paulera18 <142455846+Paulera18@users.noreply.github.com>
2024-02-16 09:54:39 -0300

    Add files via upload

---
+++ strings.php
@@ -0,0 +1,11 @@
+<?php
+
+$idade = 16;
+
+echo "Eu tenho \"$idade\" anos\n";
+
+echo PHP_EOL;
+
+echo "\t Você só pode entrar, se tiver mais de 18 anos\n";
+
+echo "<Você tem $idade anos. Pode entrar";
