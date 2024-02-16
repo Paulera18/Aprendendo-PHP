1e6fb228c963581000580f70a0f9d884a89d84ac
Paulera18 <142455846+Paulera18@users.noreply.github.com>
2024-02-16 09:50:04 -0300

    Add files via upload

---
+++ ola mundo.php
@@ -0,0 +1,19 @@
+
+<?php
+/*OLA MUNDO*/
+ echo"Ola mundo, esse é o meu canal";
+
+/*VARIAVEIS*/
+ $idade = 21;
+ echo $idade;
+
+ /*OPERACOES MATEMATICAS*/
+ $soma = 2 + 2;
+ $subtracao = 2 - 2;
+ $multiplicacao = 2 * 2;
+ $divisao = 2/2;
+
+ $doisAoCubo = 2**3;
+ $restoDivisao = 10 % 3;
+ 
+ echo $restoDivisao;
